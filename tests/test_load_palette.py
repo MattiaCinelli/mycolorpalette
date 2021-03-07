e521d1e8fe1fe7e647e6dd903f8a5819777fa79c
Mattia Cinelli <cinelli.mattia@gmail.com>
2021-03-07 22:57:27 +0000

    Turning private function into protective. Adding more testing

--- tests/test_load_palette.py
+++ tests/test_load_palette.py
@@ -3,30 +3,44 @@ import pytest
 from mypalette import LoadPalette
 
 
-def test_load_palette_class():
+@pytest.fixture(scope='session')
+def palette():
+    '''Returns a LoadPalette instance'''
+    return LoadPalette()
+
+
+def test_load_palette_class(palette):
     """
     Testing the class load_palette attributes
     """
-    palette = LoadPalette()
     assert len(palette.primary) == 3
     assert len(palette.secondary) == 3
     assert len(palette.tertiary) == 6
     assert len(palette.black_white) == 2
    
 
-# @pytest.fixture(name='palette', scope='class')
-def invoke_class():
-    a = os.path.join( 'data', 'test_palette.txt')
-    b = os.path.join( 'data', 'test_palette.json')
-    palette = LoadPalette().create_new_palette(
-        input_txt = a,
-        output_json = b
+@pytest.mark.parametrize('txt, JSON', [
+   (os.path.join(os.getcwd(), 'tests', 'data', 'test_palette.txt'),
+    os.path.join(os.getcwd(), 'tests', 'data', 'test_palette.JSON'))
+])
+def test_create_new_palette(palette, txt, JSON):
+    p = palette.create_new_palette(
+        input_txt = txt,
+        output_json = JSON
         )
+    assert type(p) == dict
+    assert len(p) == 3
+    assert list(p.keys()) == ['HEXs', 'RGBs', 'Names']
+    assert len(p['HEXs']) == 5
+    assert len(p['RGBs']) == 5
+    assert len(p['Names']) == 5
 
-def test_correct_behavior():
+
+@pytest.mark.parametrize('JSON', [
+   (os.path.join(os.getcwd(), 'tests', 'data', 'test_palette.JSON'))
+])
+def test_correct_behavior(JSON):
     """
-    Test if the method runs correctly and save the file.
+    If create_new_palette runs correctly it will save a JSON file.
     """
-    aaa = os.path.join(os.getcwd(), 'tests', 'data', 'test_palette.json')
-    assert os.path.isfile(aaa)==True
-    # assert os.path.isfile('data/test_palette.json')
+    assert os.path.isfile(JSON)==True
