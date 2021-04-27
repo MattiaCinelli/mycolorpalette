d8c3ef6d2334d64015273e172bb917675bdef7a5
Mattia Cinelli <cinelli.mattia@gmail.com>
2021-04-27 09:29:47 +0100

    Readding files

---
+++ mypalette/src/load_palette.py
@@ -0,0 +1,314 @@
+# Standard Libraries
+import os
+
+# Third party libraries
+import ast
+import json
+from typing import Union, List
+import webcolors
+import matplotlib.colors as clr
+
+# Local imports
+from mypalette.commons.logs import logging
+
+
+class LoadPalette:
+    """
+    Core class of mypalette library. After initializing this class,
+    you can create new palette or load pre existing ones.
+    You can download the color palette from coolors.co or
+    submite a list of hexadecimal code colors.
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+    primary : list of strings.
+        List of the primary web colors.
+    secondary : list of strings.
+        List of the secondary web colors.
+    tertiary : list of strings.
+        List of the tertiary web colors.
+    black_white : list of strings.
+        List of the black and white color.
+
+    Notes
+    -----
+
+    References
+    -----
+    .. [1] coolors.co: Website of reference from which to download the
+    color palette compatible with this package
+    <https://coolors.co/>`_
+    .. [2] List of web colors: usability.gov
+    <https://www.usability.gov/how-to-and-tools/methods/color-basics.html>`_
+
+    Examples
+    --------
+    >>> from mypalette import LoadPalette
+
+    >>> palette = LoadPalette()
+    >>> p = palette.create_new_palette(
+    ...     input_txt='palette.txt', output_json='palette.json')
+    >>> print(p)
+    {'HEXs': ['#000000', '#FFFFFF'], 'RGBs': [(0.0, 0.0, 0.0),
+    (1.0, 1.0, 1.0)],
+    'Names': ['black', 'white']}
+
+    >>> p = palette.load_palette(json_path='palette.json')
+    >>> print(p)
+    ['#000000', '#FFFFFF']
+
+    >>> p = palette.create_palette_from_hex_list(
+        hexadecimal = ['#000000', '#FFFFFF'],
+        output_json = 'black_and_white.json')
+    >>> print(p)
+    {'HEXs': ['#000000', '#FFFFFF'], 'RGBs': [(0.0, 0.0, 0.0),
+    (1.0, 1.0, 1.0)],
+    'Names': ['black', 'white']}
+    """
+
+    primary = ["#FF0000", "#00FF00", "#0000FF"]
+    secondary = ["#00FFFF", "#FF00FF", "#FFFF00"]
+    tertiary = ["#FF7000", "#7FFF00", "#00FF7F", "#007FFF", "#7F00FF", "#FF007F"]
+    black_white = ["#000000", "#FFFFFF"]
+    matplotlib_colors = clr.BASE_COLORS
+
+    def __init__(self):
+        # Logging
+        self.logger = logging.getLogger("Load Palette")
+
+    def __str__(self):
+        return "This class load and create stored color palette."
+
+    def _load_txt_from_coolors(self, input_txt: str):
+        """
+        Parameters
+        ----------
+        input_txt : string
+            Path to the TXT file containing the palette as
+            downloaded by coolors.co.
+
+        # Returns
+        # -------
+        # palette : dictionary
+        #     Python dictionary contains colors codes and values.
+        """
+        with open(input_txt, "r") as reader:
+            extended_array = False
+            for _, line in enumerate(reader):
+                if extended_array:
+                    return ast.literal_eval(line)
+                if "extended array" in line.lower():
+                    extended_array = True
+
+    def _save_palette(self, palette: dict, output_json: str) -> None:
+        """
+        Parameters
+        ----------
+        palette : dictionary
+            Python dictionary contains colors codes and values.
+        output_json : string
+            Path for the JSON file where to save the palette in
+            the correct format.
+        """
+        self.logger.info(
+            "Saving new {} file in: {}".format(
+                os.path.basename(output_json), os.path.dirname(output_json)
+            )
+        )
+        with open(output_json, "w") as file_:
+            json.dump(palette, file_)
+
+    def _palette_code(self, palette: dict, code: dict, json_path: str):
+        """
+        Parameters
+        ----------
+        json_path : string, default=None
+            Path to the JSON file containing
+            the palette in the correct format.
+        palette : dictionary
+            Python dictionary contains colors codes and values.
+        code : {'HEXs', 'All', 'RGBs', 'Names'}
+            Specifies the type of color code required from palette.
+            It can be 'HEXs' hexadecimal, 'RGBs' stands for
+            red, green, and blue,
+            'Names' list of the name of all color (Some colors have no name),
+            'All' all previous info in a dictionary.
+        Returns
+        -------
+        palette : dictionary
+            Python dictionary contains colors codes and
+            values in matplotlib compatible format.
+        """
+        if code == "All":
+            self.logger.info(
+                "Returning all codes from {}".format(os.path.basename(json_path))
+            )
+            palette["RGBs"] = [tuple(x) for x in palette["RGBs"]]
+            return palette
+        elif code == "RGBs":
+            self.logger.info(
+                "Returning {} {} code from {}".format(
+                    len(palette), code, os.path.basename(json_path)
+                )
+            )
+            return [tuple(x) for x in palette[code]]
+        else:
+            self.logger.info(
+                "Returning {} {} code from {}".format(
+                    len(palette), code, os.path.basename(json_path)
+                )
+            )
+            return palette[code]
+
+    def _get_compatible_codes(self, palette: dict) -> None:
+        """
+        Parameters
+        ----------
+        palette : dictionary
+            Python dictionary contains colors codes and values.
+        Returns
+        -------
+        palette : dictionary
+            Python dictionary contains colors codes and values.
+        """
+        rgb = [x["rgb"] for x in palette]
+        rgb = [tuple(y / 255 for y in x) for x in rgb]
+        return {
+            "HEXs": ["#" + x["hex"] for x in palette],
+            "RGBs": rgb,
+            "Names": [x["name"] for x in palette],
+        }
+
+    def load_palette(
+        self, json_path: str, code: str = "HEXs"
+    ) -> Union[List[str], dict]:
+        """
+        Parameters
+        ----------
+        json_path : string, default=None
+            Path to the JSON file containing
+            the palette in the correct format.
+        code : {'HEXs', 'RGBs', 'Names', 'All'}, default='HEXs'
+            Specifies the type of color code required from palette.
+            It can be 'HEXs' hexadecimal, 'RGBs' stands for
+            red, green, and blue,
+            'Names' list of the name of all color (Some colors have no name),
+            'All' all previous info in a dictionary.
+        Returns
+        -------
+        palette : list of strings or dictionary
+            It contains the colors codes and values compatible with matplotlib.
+        Examples
+        -------
+        >>> from mypalette import LoadPalette
+        >>> palette = LoadPalette()
+        >>> p = palette.load_palette(json_path='black_and_white.json')
+        >>> print(p)
+        ['#000000', '#FFFFFF']
+        """
+        with open(json_path, "r") as read_file:
+            palette = json.load(read_file)
+        palette = self._palette_code(json_path=json_path, palette=palette, code=code)
+        return palette
+
+    def create_new_palette(self, input_txt: str, output_json: str = None) -> dict:
+        """
+        Parameters
+        ----------
+        input_txt : string
+            Path to the TXT file containing the palette as
+            downloaded by coolors.co.
+        output_json : string
+            Path for the JSON file where to save
+            the palette in the correct format.
+
+        Returns
+        -------
+        palette : dictionary
+            Python dictionary contains colors codes and values.
+
+        Examples
+        -------
+        >>> from mypalette import LoadPalette
+        >>> palette = LoadPalette()
+        >>> p = palette.create_new_palette(input_txt='palette_1.txt')
+        >>> print(p)
+        {'HEXs': ['#000000', '#FFFFFF'],
+        'RGBs': [(0.0, 0.0, 0.0), (1.0, 1.0, 1.0)],
+        'Names': ['black', 'white']}
+        """
+        self.logger.info(f"Importing input_txt from {input_txt}")
+        palette = self._load_txt_from_coolors(input_txt=input_txt)
+        palette = self._get_compatible_codes(palette=palette)
+
+        if output_json is None:
+            output_json = os.path.splitext(input_txt)[0] + ".json"
+
+        self._save_palette(palette=palette, output_json=output_json)
+        return palette
+
+    def create_palette_from_hex_list(
+        self, hexadecimal: List[str], output_json: str
+    ) -> dict:
+        """
+        Given a list of hexadecimal code colors,
+        it creates a new palette saved in output_json.
+        It contains the hexadecimal and RGB codes,
+        and color names (if existing).
+
+        Parameters
+        ----------
+        hexadecimal: list of strings
+            List of hexadecimal values in format: ['#XXXXXX', ...]
+        output_json : string
+            Path for the JSON file where to save
+            the palette in the correct format.
+
+        Returns
+        -------
+        palette : dictionary
+            Python dictionary contains colors codes and values.
+
+        Examples
+        -------
+        >>> from mypalette import LoadPalette
+        >>> palette = LoadPalette()
+        >>> p = palette.create_palette_from_hex_list(
+            hexadecimal = ['#000000', '#FFFFFF'],
+            output_json = 'black_and_white.json')
+        >>> print(p)
+        {'HEXs': ['#000000', '#FFFFFF'],
+        'RGBs': [(0.0, 0.0, 0.0), (1.0, 1.0, 1.0)],
+        'Names': ['black', 'white']}
+        """
+        hexadecimal = [x if x.startswith("#") else "#" + x for x in hexadecimal]
+        names = []
+        for x in hexadecimal:
+            try:
+                names.append(webcolors.hex_to_name(x))
+            except ValueError:
+                names.append("")
+
+        rgb = [clr.to_rgb(x) for x in hexadecimal]
+
+        palette = {
+            "HEXs": hexadecimal,
+            "RGBs": rgb,
+            "Names": names,
+        }
+        self._save_palette(palette=palette, output_json=output_json)
+        return palette
+
+
+if __name__ == "__main__":
+    palette = LoadPalette()
+    p = palette.create_palette_from_hex_list(
+        ["#5fad56", "#f5e663", "#410b9b", "#ef271b", "#f9c80e"],
+        output_json="palette.json",
+    )
+    # p = palette.create_new_palette(
+    #     input_txt='palette.txt', output_json='palette.json')
+    print(p)
---
+++ mypalette/src/visualize_palette.py
@@ -0,0 +1,149 @@
+# Standard Libraries
+import os
+import math
+
+# Third party libraries
+import colorsys
+import webcolors
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as clr
+
+# Local imports
+from .src.load_palette import LoadPalette
+
+
+def _luminosity(r: float, g: float, b: float) -> float:
+    """
+    From https://www.alanzucconi.com/2015/09/30/colour-sorting/
+    Two visually different shades of blue are closer,
+    compared two two different colours with the similar intensity.
+
+    An attempt to compensate for this is by sorting directly for
+    the perceived luminosity of a colour.
+    """
+    return math.sqrt(0.241 * r + 0.691 * g + 0.068 * b)
+
+
+def _step(r: float, g: float, b: float, repetitions: int = 8) -> list:
+    """
+    From https://www.alanzucconi.com/2015/09/30/colour-sorting/
+    """
+    lum = math.sqrt(0.241 * r + 0.691 * g + 0.068 * b)
+    h, s, v = colorsys.rgb_to_hsv(r, g, b)
+    h2 = int(h * repetitions)
+    v2 = int(v * repetitions)
+    return (h2, lum, v2)
+
+
+def _sort_colors(hexs, sort_by):
+    if sort_by is None:
+        return hexs
+    colors = [list(webcolors.hex_to_rgb(x)) for x in hexs]
+    if sort_by == "luminosity":
+        colors.sort(key=lambda colors: _luminosity(*colors))
+    elif sort_by == "step":
+        colors.sort(key=lambda x: _step(x[0], x[1], x[2]))
+    else:
+        print("No sorting method has be selected.")
+    return [webcolors.rgb_to_hex(x) for x in colors]
+
+
+def _plot_text(hexs, all_colors):
+    return [
+        x + "\n\n" + y.upper() + "\n\n" + str(z)
+        for x, y, z in zip(
+            [all_colors["Names"][all_colors["HEXs"].index(x)] for x in hexs],
+            hexs,
+            [list(webcolors.hex_to_rgb(x)) for x in hexs],
+        )
+    ]
+
+
+def _text_colors(colors):
+    return [
+        ("white" if x[0] * x[1] * x[2] < 0.05 else "black")
+        for x in [clr.to_rgb(x) for x in colors]
+    ]
+
+
+def visualize_palette(
+    json_path=None,
+    sort_by=None,
+    save_plot: bool = True,
+    save_type: str = "png",
+    fig_size=None,
+):
+    """
+    This function load a palette file create by create_new_palette() and
+    visualize the colors palette.
+    Such plot can be saved in the same folder of json_path.
+
+    Parameters
+    ----------
+    json_path : string, default=None
+        Path to the JSON file containing the palette in the correct format.
+    sort_by: string, default=None
+        Options: luminosity, step.
+    save_plot : bool, default=True
+        Save figure in same folder of json_path
+    save_type : string, default='png'.
+    fig_size: tuple, default=None. Figure size in inches.
+
+    Returns
+    ----------
+    all_colors : The results from LoadPalette().load_palette()
+
+    Examples
+    --------
+    >>> from mypalette import visualize_palette
+    >>> visualize_palette(json_path = 'palette.json')
+    """
+    # Loading
+    p = LoadPalette()
+    all_colors = p.load_palette(json_path=json_path, code="All")
+
+    # Sorting colors
+    colors = _sort_colors(all_colors["HEXs"], sort_by=sort_by)
+
+    # Create text for plot
+    plot_texts = _plot_text(colors, all_colors)
+
+    # Colors for text
+    text_color = _text_colors(colors)
+
+    # Plotting
+    y_pos = np.arange(len(colors))
+    performance = [1] * len(colors)
+
+    if fig_size is None:
+        fig_size = (len(colors) * 5, 5)
+
+    fig, axes = plt.subplots(figsize=fig_size, dpi=100)
+    axes.bar(y_pos, performance, width=1.0, color=colors)
+
+    axes.spines["right"].set_visible(False)
+    axes.spines["top"].set_visible(False)
+    axes.spines["left"].set_visible(False)
+    axes.spines["bottom"].set_visible(False)
+
+    for x, (y, z) in enumerate(zip(colors, text_color)):
+        axes.text(
+            x,
+            0.5,
+            plot_texts[x],
+            color=z,
+            ha="center",
+            va="center",
+            fontsize=fig_size[1] ** 2,
+            fontweight="black",
+        )
+
+    axes.set_yticks([])
+    axes.set_xticks([])
+
+    # Saving the plot
+    if bool(save_plot) is True:
+        fig.savefig("{}.{}".format(os.path.splitext(json_path)[0], save_type))
+
+    return all_colors
